96be610ff2a0412ba94c32db741eebbf218004ed
MacroModel <MacroModel_Traject@outlook.com>
2024-12-12 15:16:19 +0800

    add unlikely attribute

--- include/fast_io_hosted/process/process/posix.h
+++ include/fast_io_hosted/process/process/posix.h
@@ -123,7 +123,7 @@ inline posix_wait_status posix_waitpid(pid_t pid)
 #if defined(__linux__) && defined(__NR_wait4)
 	system_call_throw_error(system_call<__NR_wait4, int>(pid, __builtin_addressof(status.wait_loc), 0, nullptr));
 #else
-	if (::fast_io::posix::libc_waitpid(pid, __builtin_addressof(status.wait_loc), 0) == -1)
+	if (::fast_io::posix::libc_waitpid(pid, __builtin_addressof(status.wait_loc), 0) == -1) [[unlikely]]
 	{
 		throw_posix_error();
 	}
@@ -178,12 +178,12 @@ struct io_redirector
 	{
 		return_code rc;
 		rc = redirect(0, pio.in);
-		if (rc.error)
+		if (rc.error) [[unlikely]]
 		{
 			return rc;
 		}
 		rc = redirect(1, pio.out);
-		if (rc.error)
+		if (rc.error) [[unlikely]]
 		{
 			return rc;
 		}
@@ -219,7 +219,7 @@ struct io_redirector
 		{
 			rc = sys_dup2_nothrow(d.fd, target_fd);
 		}
-		if (rc.error)
+		if (rc.error) [[unlikely]]
 		{
 			return rc;
 		}
